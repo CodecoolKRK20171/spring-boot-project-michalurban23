3e688c62efc840c9e9f26c686b34b77bcb0b8bb2
Michał Urban <michalurban23@gmail.com>
2018-01-09 13:48:10 +0100

    Add custom exception for resources not found

--- src/main/java/com/codecool/michalurban/flightconnector/exception/EntityNotFoundException.java
+++ src/main/java/com/codecool/michalurban/flightconnector/exception/EntityNotFoundException.java
@@ -1,5 +1,9 @@
 package com.codecool.michalurban.flightconnector.exception;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends RuntimeException {
 
+    public EntityNotFoundException(String message) {
+
+        super(message);
+    }
 }
