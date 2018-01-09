6d40bdc6ac8ea2bbc77c2023b0b27ca10df4b506
Michał Urban <michalurban23@gmail.com>
2018-01-09 09:38:39 +0100

    Add main class

---
+++ src/main/java/com/codecool/michalurban/flightconnector/FlightconnectorApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.michalurban.flightconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightconnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightconnectorApplication.class, args);
+	}
+}
