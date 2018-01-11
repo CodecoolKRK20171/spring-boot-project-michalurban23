e81f1e814c85e4a01abffc5a720191c4ab58a4eb
Michał Urban <michalurban23@gmail.com>
2018-01-12 00:09:59 +0100

    Add hibernate EMF bean to @configuration

--- src/main/java/com/codecool/michalurban/flightconnector/FlightconnectorApplication.java
+++ src/main/java/com/codecool/michalurban/flightconnector/FlightconnectorApplication.java
@@ -2,11 +2,24 @@ package com.codecool.michalurban.flightconnector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 public class FlightconnectorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FlightconnectorApplication.class, args);
 	}
+
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+		HibernateJpaSessionFactoryBean fact = new HibernateJpaSessionFactoryBean();
+		fact.setEntityManagerFactory(emf);
+		return fact;
+	}
+
 }
